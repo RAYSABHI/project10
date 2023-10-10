2022627b4eb6af798985a4b1b4fbc51a7a8047ae
RAYSABHI <parsaiabhi143@gmail.com>
2023-10-09 19:07:53 -0700

    project10

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Abhishek Parsai
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
